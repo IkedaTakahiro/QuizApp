a0aecbc5fa1b8367bbb7fab4b8dcd83f0b8232c6
池田享浩 <ikedatakahiro@ikedatooruhiroshi-no-MacBook-Air.local>
2015-12-10 07:06:34 +0900

    Initial Commit

---
+++ QuizApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QuizApp
+//
+//  Created by 池田享浩 on 2015/12/10.
+//  Copyright (c) 2015年 takahiro ikeda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QuizApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QuizApp
+//
+//  Created by 池田享浩 on 2015/12/10.
+//  Copyright (c) 2015年 takahiro ikeda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
